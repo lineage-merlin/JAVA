a0d9117d43d001948fd428eeead2850e1e3bc25c
meraj-11ahmed <72243121+meraj-11ahmed@users.noreply.github.com>
2020-10-02 11:12:55 +0530

    Using Predefined methods in string i

--- Strings/Reverse.java
+++ Strings/Reverse.java
@@ -2,18 +2,15 @@ import java.util.*;
 public class Reverse{
     public static void main(String args[]){
         String x=new String();
+        String rev="";
         
         System.out.println("Enter the string");
         Scanner name=new Scanner(System.in);
         x=name.nextLine();
-        char w[]=new char[x.length()];
-        for(int i=0;i<x.length();i++){
-            w[i]=x.charAt(i);
+        for(int i=x.length()-1; i>=0; i--)
+        {
+            rev=rev+x.charAt(i);
         }
-        for(int i=w.length-1;i>=0;i--){
-            System.out.printf("%c",w[i]);
-        
-        }
-        System.out.printf("\n");
+        System.out.println(rev);
     }
 }
